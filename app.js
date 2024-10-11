221703f96a5ead86cfe838bbd857ccdf4d3ef580
Jess Mullins <jessem013@gmail.com>
2024-10-11 18:54:29 +1100

    commented out seed data - finished logic for api database backend

--- app.js
+++ app.js
@@ -28,35 +28,32 @@ app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
 })
 
-// Dummy data for the menu
-const menuItems = [
-    {
-      name: "Chicken Salad",
-      price: 12.99, 
-      category: "Lunch",
-      description: "A tasty salad with grilled chicken and fresh greens",
-      ingredients: ["Lettuce", "Cucumber", "Chicken", "Avocado", "Basil"],
-      available: true,
-      isVegan:  false,
-      isVegetarian: false,
-      imageUrl: "fakeImg.com"
-    }
-  ];
+// // Dummy data for the menu
+// const menuItems = [
+//     {
+//       name: "Chicken Salad",
+//       price: 12.99, 
+//       category: "Lunch",
+//       description: "A tasty salad with grilled chicken and fresh greens",
+//       ingredients: ["Lettuce", "Cucumber", "Chicken", "Avocado", "Basil"],
+//       available: true,
+//       isVegan:  false,
+//       isVegetarian: false,
+//       imageUrl: "fakeImg.com"
+//     }
+//   ];
   
-  // Function to seed the database
-  const seedDatabase = async () => {
+//   // Function to seed the database
+//   const seedDatabase = async () => {
 
-    await MenuItem.deleteMany(); // This deletes any existing documents in the collection
-    console.log('Existing menu items cleared');
-      // Add new dummy data
-      await MenuItem.insertMany(menuItems);
-      console.log('Dummy menu items added successfully');
-      // Fetch and log the added items
-      const findResult = await MenuItem.find({});
-      console.log('Menu items found:', findResult);
+//     await MenuItem.deleteMany(); // This deletes any existing documents in the collection
+//     console.log('Existing menu items cleared');
+//       // Add new dummy data
+//       await MenuItem.insertMany(menuItems);
+//       console.log('Dummy menu items added successfully');
+//       // Fetch and log the added items
+//       const findResult = await MenuItem.find({});
+//       console.log('Menu items found:', findResult);
+//   };
   
-      // Close the connection
-
-  };
-  
-  seedDatabase();
+//   seedDatabase();
